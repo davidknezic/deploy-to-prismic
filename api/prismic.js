8ecbd4bf37e3863a88a2a48e9e7b63b3248e53e5
David Knezić <davidknezic@gmail.com>
2017-06-26 23:52:39 +0200

    Move Prismic API code to separate module, fix custom type endpoint

---
+++ api/prismic.js
@@ -0,0 +1,116 @@
+const remote = require('../contento-remote')
+
+const extractToken = header => (header || '').toString().slice('Bearer '.length)
+
+export const login = (req, res) => {
+  remote.login({
+    cluster: 'https://prismic.io',
+    email: req.body.email,
+    password: req.body.password,
+  })
+  .then(session => {
+    const token = new Buffer(session).toString('base64')
+
+    res.cookie('prismicToken', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 300, // cookie lasts 300 days
+      httpOnly: true,
+      signed: true,
+      // secure: true,
+    })
+
+    res.status(200).json({
+      token,
+    })
+  })
+  .catch(err => {
+    console.log(err.message)
+    res.status(401).send('Wrong credentials')
+  })
+}
+
+export const logout = (req, res) => {
+  res.clearCookie('prismicToken')
+  res.status(200).end()
+}
+
+export const profile = (req, res) => {
+  const token = extractToken(req.headers.authorization)
+
+  remote.me({
+    cluster: 'https://prismic.io',
+    session: new Buffer(token, 'base64').toString('utf8')
+  })
+  .then(profile => {
+    res.status(200).json(profile)
+  })
+  .catch(err => {
+    console.log(err.message)
+    res.status(500).send('Internal error')
+  })
+}
+
+export const listRepositories = (req, res) => {
+  const token = extractToken(req.headers.authorization)
+
+  remote.listRepositories({
+    cluster: 'https://prismic.io',
+    session: new Buffer(token, 'base64').toString('utf8')
+  })
+  .then(repositories => repositories.map(repository => {
+    const pattern = /:\/\/([^\.]+)/
+
+    return {
+      ...repository,
+      displayName: repository.name,
+      name: repository.url.match(pattern)[1],
+    }
+  }))
+  .then(repositories => {
+    res.status(200).json(repositories)
+  })
+  .catch(err => {
+    console.log(err.message)
+    res.status(500).send('Internal error')
+  })
+}
+
+export const createRepository = (req, res) => {
+  const token = extractToken(req.headers.authorization)
+
+  remote.addRepository({
+    cluster: 'https://prismic.io',
+    session: new Buffer(token, 'base64').toString('utf8'),
+    repository: req.body.name,
+  })
+  .then(isSuccessful => {
+    res.status(200).json({
+      url: `http://${req.body.name}.prismic.io`,
+      name: req.body.name,
+    })
+  })
+  .catch(err => {
+    console.log(err.message)
+    res.status(500).send('Internal error')
+  })
+}
+
+export const createCustomType = (req, res) => {
+  const token = extractToken(req.headers.authorization)
+
+  remote.saveMask({
+    maskName: req.body.name,
+    maskJSON: JSON.stringify(req.body.customType),
+    cluster: 'https://prismic.io',
+    session: new Buffer(token, 'base64').toString('utf8'),
+    repository: req.body.repository,
+  })
+  .then(isSuccessful => {
+    res.status(200).json({
+      status: 'success',
+    })
+  })
+  .catch(err => {
+    console.log(err.message)
+    res.status(500).send('Internal error')
+  })
+}
--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const next = require('next')
-const remote = require('./contento-remote')
 
 const parseSource = require('./api/parse-source')
+const {
+  createCustomType,
+  createRepository,
+  listRepositories,
+  login,
+  logout,
+  profile,
+} = require('./api/prismic')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -12,7 +19,6 @@ const handle = app.getRequestHandler()
 
 app.prepare()
 .then(() => {
-  const extractToken = header => (header || '').toString().slice('Bearer '.length)
   const server = express()
 
   server.use(bodyParser.urlencoded({
@@ -21,36 +27,8 @@ app.prepare()
   server.use(bodyParser.json())
   server.use(cookieParser('change it'))
 
-  server.post('/login', (req, res) => {
-    remote.login({
-      cluster: 'https://prismic.io',
-      email: req.body.email,
-      password: req.body.password,
-    })
-    .then(session => {
-      const token = new Buffer(session).toString('base64')
-
-      res.cookie('prismicToken', token, {
-        maxAge: 1000 * 60 * 60 * 24 * 300, // cookie lasts 300 days
-        httpOnly: true,
-        signed: true,
-        // secure: true,
-      })
-
-      res.status(200).json({
-        token,
-      })
-    })
-    .catch(err => {
-      console.log(err.message)
-      res.status(401).send('Wrong credentials')
-    })
-  })
-
-  server.post('/logout', (req, res) => {
-    res.clearCookie('prismicToken')
-    res.status(200).end()
-  })
+  server.post('/login', login)
+  server.post('/logout', logout)
 
   server.get('/api/source', (req, res) => {
     const url = req.query.url
@@ -66,82 +44,12 @@ app.prepare()
       })
   })
 
-  server.get('/api/profile/me', (req, res) => {
-    const token = extractToken(req.headers.authorization)
+  server.get('/api/profile/me', profile)
+  server.get('/api/repositories', listRepositories)
+  server.post('/api/repositories', createRepository)
+  server.post('/api/custom-type', createCustomType)
 
-    remote.me({
-      cluster: 'https://prismic.io',
-      session: new Buffer(token, 'base64').toString('utf8')
-    })
-    .then(profile => {
-      res.status(200).json(profile)
-    })
-    .catch(err => {
-      console.log(err.message)
-      res.status(500).send('Internal error')
-    })
-  })
-
-  server.get('/api/repositories', (req, res) => {
-    const token = extractToken(req.headers.authorization)
-
-    remote.listRepositories({
-      cluster: 'https://prismic.io',
-      session: new Buffer(token, 'base64').toString('utf8')
-    })
-    .then(repositories => {
-      res.status(200).json(repositories)
-    })
-    .catch(err => {
-      console.log(err.message)
-      res.status(500).send('Internal error')
-    })
-  })
-
-  server.post('/api/repositories', (req, res) => {
-    const token = extractToken(req.headers.authorization)
-
-    remote.addRepository({
-      cluster: 'https://prismic.io',
-      session: new Buffer(token, 'base64').toString('utf8'),
-      repository: req.body.name,
-    })
-    .then(isSuccessful => {
-      res.status(200).json({
-        url: `http://${req.body.name}.prismic.io`,
-        name: req.body.name,
-      })
-    })
-    .catch(err => {
-      console.log(err.message)
-      res.status(500).send('Internal error')
-    })
-  })
-
-  server.post('/api/mask', (req, res) => {
-    const token = extractToken(req.headers.authorization)
-
-    remote.saveMask({
-      maskName: req.body.name,
-      maskJSON: req.body.mask,
-      cluster: 'https://prismic.io',
-      session: new Buffer(token, 'base64').toString('utf8'),
-      repository: req.body.repository,
-    })
-    .then(isSuccessful => {
-      res.status(200).json({
-        mask: req.body.name,
-      })
-    })
-    .catch(err => {
-      console.log(err.message)
-      res.status(500).send('Internal error')
-    })
-  })
-
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+  server.get('*', (req, res) => handle(req, res))
 
   server.listen(3000, (err) => {
     if (err) throw err
